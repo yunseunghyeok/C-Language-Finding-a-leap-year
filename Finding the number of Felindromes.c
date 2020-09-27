4cd55599cb90962185d96ec788a86039e435ad6c
yunseunghyeok <71643070+yunseunghyeok@users.noreply.github.com>
2020-09-27 19:28:11 +0900

    Update Finding the number of Felindromes.c

--- Finding the number of Felindromes.c
+++ Finding the number of Felindromes.c
@@ -1,38 +1,50 @@
 #include<stdio.h>
 #include<math.h>
-main() {
-	int input, length = -1, frontnum, backnum, a;
+
+int main() {
+	int input, length = -1, frontnum, backnum;
 	scanf_s("%d", &input);
-	int num = input;
-	int num2 = input;
-	int num3 = input;
-	while (num > 0)
+	while (input != 0)
 	{
-		num /= 10;
-		length++;
+		int a = 1;
+		if (input >= 1 && input <= 99999)
+		{
+			int num = input;
+			int num2 = input;
+			int num3 = input;
+			while (num > 0)
+			{
+				num /= 10;
+				length++;
+			}
+				while (num2 > 0)
+				{
+					frontnum = num2 % 10;
+					num2 /= 10;
+					backnum = num3;
+					backnum /= (int)pow(10, length);
+					num3 %= (int)pow(10, length);
+					length--;
+					if (a == 1)
+					{
+						if (frontnum == backnum)
+							a = 1;
+						else
+							a = 0;
+					}
+				}
+			switch (a)
+			{
+			case 0:
+				printf("no\n");
+				break;
+			case 1:
+				printf("yes\n");
+				break;
+			}
+		}
+		scanf_s("%d", &input);
 	}
-	while (num2 > 0)
-	{
-		frontnum = num2 % 10;
-		num2 /= 10;
-		printf("%d\n", frontnum);
-		backnum = num3;
-		backnum /= (int)pow(10, length);
-		num3 %= (int)pow(10, length);
-		length--;
-		printf("%d\n", backnum);
-		if (frontnum == backnum)
-			a = 1;
-		else
-			a = 0;
-	}
-	switch (a)
-	{
-		case 0:
-			printf("no");
-			break;
-		case 1:
-			printf("yes");
-			break;
-	}
+
+	return 0;
 }
